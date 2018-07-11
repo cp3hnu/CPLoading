04b719511419917c3a317ca6d83f0fb1cefe0e3f
WeiZhao <wei.zhao-1@datayp.com>
2018-07-11 14:34:06 +0800

    create framework

--- Example/CPLoading/UsageViewController.swift
+++ Example/CPLoading/UsageViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CPLoading
 
 class UsageViewController: UIViewController, URLSessionDownloadDelegate {
 
---
+++ Example/Framework/CPLoading.h
@@ -0,0 +1,19 @@
+//
+//  CPLoading.h
+//  CPLoading
+//
+//  Created by CP3 on 2018/7/11.
+//  Copyright © 2018年 CSDEPT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CPLoading.
+FOUNDATION_EXPORT double CPLoadingVersionNumber;
+
+//! Project version string for CPLoading.
+FOUNDATION_EXPORT const unsigned char CPLoadingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CPLoading/PublicHeader.h>
+
+
